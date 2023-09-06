23100ada009649ecd39a977316f05e5f5ae75257
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-06 13:57:40 -0400

    Drew some shapes onto the canvas

--- experiments/drawing/drawing-experiments/js/script.js
+++ experiments/drawing/drawing-experiments/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Drawing Experiments
+ * Ryan Bujold
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -20,7 +20,23 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(500,500);
 
+    background(100,255,100);
+    rect(50,50,200,300);
+    line(0,0,300,500);
+    line(250,0,250,500);
+    ellipse(250,250,100,50);
+    rectMode(CENTER);
+    rect(250,250,30,30);
+    point(250,250);
+    ellipseMode(CORNER);
+    ellipse(400,250,40,60);
+    ellipse(400,250,40,50);
+    ellipse(400,250,40,40);
+    ellipse(400,250,40,30);
+    ellipse(400,250,40,20);
+    ellipse(400,250,40,10);
 }
 
 
