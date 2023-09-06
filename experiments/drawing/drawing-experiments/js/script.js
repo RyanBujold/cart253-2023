c0948c0edc678d43cc08d0cac696be0887f07141
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-06 14:12:38 -0400

    adding colors to shapes

--- experiments/drawing/drawing-experiments/js/script.js
+++ experiments/drawing/drawing-experiments/js/script.js
@@ -23,19 +23,32 @@ function setup() {
     createCanvas(500,500);
 
     background(100,255,100);
+
+    stroke(255,0,255);
+    fill(0,255,100);
+
     rect(50,50,200,300);
     line(0,0,300,500);
     line(250,0,250,500);
     ellipse(250,250,100,50);
+
+    fill(0,100,255,100);
     rectMode(CENTER);
     rect(250,250,30,30);
     point(250,250);
+
+    noStroke();
     ellipseMode(CORNER);
     ellipse(400,250,40,60);
+    fill(0,100,100,100);
     ellipse(400,250,40,50);
+    fill(0,150,150,100);
     ellipse(400,250,40,40);
+    fill(0,200,200,100);
     ellipse(400,250,40,30);
+    fill(0,255,255,100);
     ellipse(400,250,40,20);
+    fill(255,255,255,100);
     ellipse(400,250,40,10);
 }
 
