93b995d0ca6259db68b7473eb22a0f16941d5d24
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-07 15:10:11 -0400

    drawing the canvas and alien's body

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -1,9 +1,9 @@
 /**
- * Title of Project
- * Author Name
+ * Drawing an alien
+ * Ryan Bujold
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Using javascript and p5 drawing instructions to 
+ * draw an alien on a canvas.
  */
 
 "use strict";
@@ -17,9 +17,26 @@ function preload() {
 
 
 /**
- * Description of setup
+ * Making a canvas and drawing shapes onto it.
 */
 function setup() {
+    // Variables for the canva's width and height
+    var canvasWidth = 600;
+    var canvasHeight = 600;
+
+    // Create the canvas and light blue background 
+    createCanvas(canvasWidth,canvasHeight);
+    background(3, 202, 252);
+    // Make sure we don't draw any lines around our shapes
+    noStroke();
+
+    // Draw the aliens body
+    fill(168, 172, 173);
+    ellipse(canvasWidth/2, canvasHeight, 500);
+    fill(54, 227, 57);
+    quad(150, 400, canvasWidth/2, 500, 450, 400, canvasWidth/2, 300);
+
+
 
 }
 
