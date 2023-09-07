8dfcd70c590aaf5d6dc4e91f69c38d577d1adfb5
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-07 15:31:06 -0400

    drawing the alien's head and removing the canvas width and height variables

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -20,23 +20,24 @@ function preload() {
  * Making a canvas and drawing shapes onto it.
 */
 function setup() {
-    // Variables for the canva's width and height
-    var canvasWidth = 600;
-    var canvasHeight = 600;
-
     // Create the canvas and light blue background 
-    createCanvas(canvasWidth,canvasHeight);
+    createCanvas(600,700);
     background(3, 202, 252);
     // Make sure we don't draw any lines around our shapes
     noStroke();
 
     // Draw the aliens body
-    fill(168, 172, 173);
-    ellipse(canvasWidth/2, canvasHeight, 500);
-    fill(54, 227, 57);
-    quad(150, 400, canvasWidth/2, 500, 450, 400, canvasWidth/2, 300);
+    fill(168, 172, 173);//gray
+    ellipse(300, 700, 500);
+    fill(50, 209, 53);//dark green
+    quad(150, 500, 300, 650, 450, 500, 300, 400);
 
-
+    // Draw the alien's head with a shadow underneath
+    ellipseMode(CENTER);
+    fill(0,0,0,50);//black with transparency
+    ellipse(300,270,360,500);
+    fill(54, 227, 57);//green
+    ellipse(300,250,400,500);
 
 }
 
