ab509819c997b1e40c761fbb3bcb189db9b94218
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-12 16:33:07 -0400

    started activity 3: moving pictures

---
+++ activities/03-moving-pictures/template-p5-project/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Moving Pictures
+ * Ryan Bujold
+ * 
+ * Making a simple abstract animation on a canvas.
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
