9ea1fb87524d8c70111ede84a5f53676ee4bd9bf
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-12 16:55:52 -0400

    drawing the circles moving towards the center of the canvas

--- activities/03-moving-pictures/template-p5-project/js/script.js
+++ activities/03-moving-pictures/template-p5-project/js/script.js
@@ -44,16 +44,33 @@ function preload() {
 
 
 /**
- * Drawing the canvas
+ * Setting up the canvas
 */
 function setup() {
-
+    // Creating the canvas
+    createCanvas(canvasWidth,canvasHeight);
+    noStroke();
 }
 
 
 /**
- * Description of draw()
+ * Drawing on the canvas each frame
 */
 function draw() {
+    // Coloring in the background
+    background(bg.red,bg.green,bg.blue);
+    bg.blue += 1;
+
+    // Circle 1
+    fill(circle1.fill, circle1.alpha);
+    ellipse(circle1.x, circle1.y, circle1.size);
+    circle1.x += 1;
+    circle1.x = constrain(circle1.x, 0, canvasWidth/2);
+
+    // Circle 2
+    fill(circle2.fill, circle2.alpha);
+    ellipse(circle2.x, circle2.y, circle2.size);
+    circle2.x -= 1;
+    circle2.x = constrain(circle2.x, canvasWidth/2, canvasWidth);
 
 }
