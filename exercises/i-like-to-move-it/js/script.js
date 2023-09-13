afa57e2f75789153609db7e1235cef40756ecc17
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-13 16:30:03 -0400

    add a pyramid (triangle) that follows the mouse pointer

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -33,13 +33,13 @@ let ball = {
     bounce:20,
 }
 
-let triangle = {
-    x1:10,
-    y1:10,
-    x2:20,
-    y2:10,
-    x3:15,
-    y3:20,
+let pyramid = {
+    x1:100,
+    y1:50,
+    x2:canvasWidth/2 - 50,
+    y2:canvasHeight/2,
+    x3:canvasWidth/2 + 50,
+    y3:canvasHeight/2,
     red:0,
     green:255,
     blue:255,
@@ -67,6 +67,14 @@ function setup() {
 */
 function draw() {
     background(0,50,200);
+
+    // Triangle
+    fill(pyramid.red, pyramid.green, pyramid.blue);
+    triangle(pyramid.x1, pyramid.y1, pyramid.x2, pyramid.y2, pyramid.x3, pyramid.y3);
+    pyramid.x1 = mouseX;
+    pyramid.y1 = mouseY;
+    pyramid.x1 = constrain(mouseX, 0, canvasWidth);
+    pyramid.y1 = constrain(mouseY, 0, canvasHeight);
 
     // Ball
     fill(ball.red, ball.green, ball.blue);
