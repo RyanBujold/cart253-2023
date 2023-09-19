22d0fab8c19731d8b138274863ca6fa8400553ca
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-19 16:51:50 -0400

    began work on activity 04: dodging covid 19

---
+++ activities/04-dodging-covid-19/template-p5-project/js/script.js
@@ -0,0 +1,46 @@
+/**
+ * Dodging COVID-19
+ * Ryan Bujold
+ * 
+ * A simulation of dodging covid-19
+ */
+
+"use strict";
+
+// A covid 19 object
+let covid19 = {
+    x:0,
+    y:250,
+    size:100,
+    vx:0,
+    vy:0,
+    speed:5,
+    fill:{
+        r:255,
+        g:0,
+        b:0,
+    }
+}
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Setup our canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
