4a80706af95251d414c7a24fd9c56b3db59b5d2a
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-19 18:03:33 -0400

    added more covids that change size and modified the static effect

--- activities/04-dodging-covid-19/template-p5-project/js/script.js
+++ activities/04-dodging-covid-19/template-p5-project/js/script.js
@@ -22,6 +22,8 @@ let covid19 = {
     }
 }
 
+let covidArray = [covid19];
+
 // A User object
 let user = {
     x:0,
@@ -31,7 +33,7 @@ let user = {
 }
 
 // the static amound
-let staticAmount = 1000;
+let staticAmount = 50;
 
 /**
  * Description of preload
@@ -60,35 +62,67 @@ function draw() {
     background(0,0,0);
 
     // -- Static --
-    for(let i = 0; i < staticAmount; i++){
-        let x = random(0,width);
-        let y = random(0,height);
-        stroke(255,0,0);
-        point(x,y);
-    }
+    // Draw a static trail behind each covid
+    covidArray.forEach(function(currentValue){
+        let covid = currentValue;
+        for(let i = 0; i < staticAmount; i++){
+            let x = random(0,covid.x);
+            let y = random(covid.y-covid.size/2, covid.y+covid.size/2);
+            stroke(255,0,0);
+            point(x,y);
+        }
+    });
 
     // -- User --
+    noStroke();
     user.x = mouseX;
     user.y = mouseY;
     fill(user.fill);
     ellipse(user.x, user.y, user.size);
 
     // -- Covid 19 --
-    covid19.x += covid19.vx;
-    covid19.y += covid19.vy;
-    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
-    noStroke();
-    ellipse(covid19.x, covid19.y, covid19.size);
-    // If the covid reaches the edge of the screen,
-    // reset it back to the left side of the window
-    if(covid19.x > width){
-        covid19.x = 0;
-        covid19.y = random(0, height);
-    }
+    // Perform logic for every covid 19
+    covidArray.forEach(function(currentValue){
 
-    // -- Collision Checking --
-    let d = dist(user.x, user.y, covid19.x, covid19.y);
-    if(d < (covid19.size/2) + (user.size/2)){
-        noLoop();
-    }
+        let covid = currentValue;
+
+        // -- Collision Checking --
+        let d = dist(user.x, user.y, covid.x, covid.y);
+        if(d < (covid.size/2) + (user.size/2)){
+            noLoop();
+        }
+
+        covid.x += covid.vx;
+        covid.y += covid.vy;
+        fill(covid.fill.r, covid.fill.g, covid.fill.b);
+        noStroke();
+        ellipse(covid.x, covid.y, covid.size);
+        // If the covid reaches the edge of the screen,
+        // reset it back to the left side of the window
+        if(covid.x > width){
+            covid.x = 0;
+            covid.y = random(0, height);
+            //Add a new covid to the array
+            let temp = {
+                x:0,
+                y:random(0, height),
+                size:random(20,150),
+                vx:0,
+                vy:0,
+                speed:random(1,8),
+                fill:{
+                    r:255,
+                    g:0,
+                    b:0,
+                }
+            }
+            temp.vx = temp.speed;
+            covidArray.push(temp);
+
+            // Make the user smaller for every additional covid
+            user.size -= 1;
+            user.size = constrain(user.size, 1, 100);
+        }
+    });
+    
 }
