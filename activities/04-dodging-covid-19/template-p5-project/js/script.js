895cf0e68c214d75162e6013217d89cf27f4cc41
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-19 17:05:52 -0400

    Added a user controlled circle

--- activities/04-dodging-covid-19/template-p5-project/js/script.js
+++ activities/04-dodging-covid-19/template-p5-project/js/script.js
@@ -22,6 +22,14 @@ let covid19 = {
     }
 }
 
+// A User object
+let user = {
+    x:0,
+    y:0,
+    size:100,
+    fill:255,
+}
+
 /**
  * Description of preload
 */
@@ -48,9 +56,21 @@ function setup() {
 function draw() {
     background(0,0,0);
 
+    // User
+    user.x = mouseX;
+    user.y = mouserY;
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+
+    // Covid 19
     covid19.x += covid19.vx;
     covid19.y += covid19.vy;
     fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
     noStroke();
     ellipse(covid19.x, covid19.y, covid19.size);
+
+    if(covid19.x > width){
+        covid19.x = 0;
+        covid19.y = random(0, height);
+    }
 }
