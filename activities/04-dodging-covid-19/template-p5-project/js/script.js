4a129f2b5af18abdb731d3d30dfeab551ab8d4fc
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-19 17:17:09 -0400

    added static and an end condition when the user touches a covid

--- activities/04-dodging-covid-19/template-p5-project/js/script.js
+++ activities/04-dodging-covid-19/template-p5-project/js/script.js
@@ -30,6 +30,9 @@ let user = {
     fill:255,
 }
 
+// the static amound
+let staticAmount = 1000;
+
 /**
  * Description of preload
 */
@@ -56,21 +59,36 @@ function setup() {
 function draw() {
     background(0,0,0);
 
-    // User
+    // -- Static --
+    for(let i = 0; i < staticAmount; i++){
+        let x = random(0,width);
+        let y = random(0,height);
+        stroke(255,0,0);
+        point(x,y);
+    }
+
+    // -- User --
     user.x = mouseX;
-    user.y = mouserY;
+    user.y = mouseY;
     fill(user.fill);
     ellipse(user.x, user.y, user.size);
 
-    // Covid 19
+    // -- Covid 19 --
     covid19.x += covid19.vx;
     covid19.y += covid19.vy;
     fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
     noStroke();
     ellipse(covid19.x, covid19.y, covid19.size);
-
+    // If the covid reaches the edge of the screen,
+    // reset it back to the left side of the window
     if(covid19.x > width){
         covid19.x = 0;
         covid19.y = random(0, height);
+    }
+
+    // -- Collision Checking --
+    let d = dist(user.x, user.y, covid19.x, covid19.y);
+    if(d < (covid19.size/2) + (user.size/2)){
+        noLoop();
     }
 }
