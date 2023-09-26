052947fdadadee35f5ecba2dea721a145484bfaf
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-26 10:24:15 -0400

    drew a fish to the canvas

--- projects/project1/template-p5-project/js/script.js
+++ projects/project1/template-p5-project/js/script.js
@@ -7,8 +7,21 @@
 
 "use strict";
 
+let fish = {
+    x:500,
+    y:500,
+    w:300,
+    h:150,
+    fill:{
+        r:0,
+        g:0,
+        b:200,
+    },
+    alpha:100,
+}
+
 /**
- * Description of preload
+ * Load files
 */
 function preload() {
 
@@ -16,16 +29,28 @@ function preload() {
 
 
 /**
- * Description of setup
+ * Set up the canvas
 */
 function setup() {
     createCanvas(windowWidth,windowHeight);
+    noStroke();
 }
 
 
 /**
- * Description of draw()
+ * Draw objects on the canvas
 */
 function draw() {
     background(59, 126, 235);
+
+    // -- Draw a fish --
+    // Draw the body
+    fill(fish.fill.r, fish.fill.g, fish.fill.b);
+    ellipse(fish.x, fish.y, fish.w, fish.h);
+    // Draw the fin
+    let fishEndX = fish.x - fish.w/2 + 20;
+    quad(fishEndX, fish.y, fishEndX -100, fish.y -100, fishEndX -50, fish.y, fishEndX -100, fish.y +100);
+    // Draw the eye
+    fill(0);
+    ellipse(fish.x + 100, fish.y, 30);
 }
