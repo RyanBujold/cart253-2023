7a852a430051545e97c1c1b249870a3091d9591c
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-26 10:31:31 -0400

    made a swimming fish

--- projects/project1/template-p5-project/js/script.js
+++ projects/project1/template-p5-project/js/script.js
@@ -18,6 +18,8 @@ let fish = {
         b:200,
     },
     alpha:100,
+    isFacingRight:true,
+    speed:5,
 }
 
 /**
@@ -43,14 +45,42 @@ function setup() {
 function draw() {
     background(59, 126, 235);
 
-    // -- Draw a fish --
+    // -- Fish --
+    // Move the fish
+    if(fish.isFacingRight){
+        // Move to the right
+        fish.x+=fish.speed;
+        // If we reach the edge turn around
+        if(fish.x > windowWidth){
+            fish.isFacingRight = false;
+        }
+    }
+    else {
+        // Move to the left
+        fish.x-=fish.speed;
+        // If we reach the edge turn around
+        if(fish.x < 0){
+            fish.isFacingRight = true;
+        }
+    }
     // Draw the body
     fill(fish.fill.r, fish.fill.g, fish.fill.b);
     ellipse(fish.x, fish.y, fish.w, fish.h);
-    // Draw the fin
-    let fishEndX = fish.x - fish.w/2 + 20;
-    quad(fishEndX, fish.y, fishEndX -100, fish.y -100, fishEndX -50, fish.y, fishEndX -100, fish.y +100);
-    // Draw the eye
-    fill(0);
-    ellipse(fish.x + 100, fish.y, 30);
+    if(fish.isFacingRight){
+        // Draw the fin
+        let fishEndX = fish.x - fish.w/2 + 20;
+        quad(fishEndX, fish.y, fishEndX -100, fish.y -100, fishEndX -50, fish.y, fishEndX -100, fish.y +100);
+        // Draw the eye
+        fill(0);
+        ellipse(fish.x + 100, fish.y, 30);
+    }
+    else {
+        // Draw the fin
+        let fishEndX = fish.x + fish.w/2 - 20;
+        quad(fishEndX, fish.y, fishEndX +100, fish.y -100, fishEndX +50, fish.y, fishEndX +100, fish.y +100);
+        // Draw the eye
+        fill(0);
+        ellipse(fish.x - 100, fish.y, 30);
+    }
+    
 }
