f2a574f26e6e07539a53b75114b962e19844470c
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-26 17:09:11 -0400

    added functions and finished the activity

--- activities/05-looking-for-love/template-p5-project/js/script.js
+++ activities/05-looking-for-love/template-p5-project/js/script.js
@@ -27,6 +27,8 @@ let circle2 = {
     speed:5,
 }
 
+let state = "title";
+
 /**
  * Load files
 */
@@ -55,41 +57,105 @@ function setup() {
 */
 function draw() {
     background(0);
+    switch(state){
+        case "simulation":
+            simulation();
+            break;
+        case "title":
+            title();
+            break;
+        case "love":
+            love();
+            break;
+        case "sadness":
+            sadness();
+            break;
+    }
+}
 
-    // Move the circles
+// Move the circles
+function move(){
     circle1.x += circle1.vx;
     circle1.y += circle1.vy;
     circle2.x += circle2.vx;
     circle2.y += circle2.vy;
-    // Draw the circles
-    fill(255);
-    ellipse(circle1.x, circle1.y, circle1.size);
-    ellipse(circle2.x, circle2.y, circle2.size);
-
-    if(dist(circle1.x, circle1.y, circle2.x, circle2.y) <= circle1.size || 
-       dist(circle1.x, circle1.y, circle2.x, circle2.y) <= circle2.size ){
-       // True Love!
-    }
-    else if(isCircleTouchingEdge(circle1.x, circle1.y, circle1.size)){
-        // Play the sad ending
-    }
-    else if (isCircleTouchingEdge(circle2.x, circle2.y, circle2.size)){
-        // Play the sad ending
-    }
-
 }
 
 // Returns true if the circle is touching an edge of the canvas.
 // Otherwise returns false.
-function isCircleTouchingEdge(x, y, size){
+function checkOffScreen(x, y, size){
     let radius = size/2;
     if(x + radius > canvasWidth || x < 0){
+        state = "sadness";
         return true;
     }
     else if(y + radius > canvasHeight || y < 0){
+        state = "sadness";
         return true;
     }
     else{
         return false;
     }
+}
+
+// Return true if the circles overlap, otherwise return false.
+function checkOverlap(x1, y1, x2, y2, size1, size2){
+    let d = dist(x1, y1, x2, y2);
+    if(d < (size1/2) + (size2/2)){
+        state = "love";
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+// Draw the circles
+function display(){
+    fill(255);
+    ellipse(circle1.x, circle1.y, circle1.size);
+    ellipse(circle2.x, circle2.y, circle2.size);
+}
+
+// Mouse press logic
+function mousePressed(){
+    if(state == "title"){
+        state = "simulation";
+    }
+}
+
+// Perform the simulation
+function simulation(){
+    move();
+    if(checkOverlap(circle1.x, circle1.y, circle2.x, circle2.y, circle1.size, circle2.size)){
+       // True Love!
+    }
+    else if(checkOffScreen(circle1.x, circle1.y, circle1.size)){
+        // Play the sad ending
+    }
+    else if (checkOffScreen(circle2.x, circle2.y, circle2.size)){
+        // Play the sad ending
+    }
+    display();
+}
+
+// Display the title state
+function title(){
+    textSize(50);
+    fill(255);
+    text("LOVE?", windowWidth/3, windowHeight/3);
+}
+
+// True love ending
+function love(){
+    textSize(50);
+    fill(237, 64, 231);
+    text("LOVE!", windowWidth/3, windowHeight/3);
+}
+
+// Sadness ending
+function sadness(){
+    textSize(50);
+    fill(73, 142, 245);
+    text("D:", windowWidth/3, windowHeight/3);
 }
