dc76cbbeb5750dc273a0f1ffa9b136962e01cea5
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-26 16:01:35 -0400

    started "looking for love" activity

---
+++ activities/05-looking-for-love/template-p5-project/README.md
@@ -0,0 +1,4 @@
+# Looking for love
+## Ryan Bujold
+
+A simulation where two circles meets in the darkness to create love.
---
+++ activities/05-looking-for-love/template-p5-project/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Looking for Love
+ * Ryan Bujold
+ * 
+ * Making a simulation of love with randomly moving circles.
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
