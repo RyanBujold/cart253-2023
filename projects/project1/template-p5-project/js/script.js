793d11ddd4bc5ae8f937e089ddee81525759e6df
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-09-28 18:53:37 -0400

    added fish catching logic and random fish

--- projects/project1/template-p5-project/js/script.js
+++ projects/project1/template-p5-project/js/script.js
@@ -20,20 +20,22 @@ let fish = {
         g:0,
         b:200,
     },
-    alpha:100,
     isFacingRight:true,
     speed:5,
+    isCaught:false,
 }
 
+// A fishing line
 let fishingLine = {
     x1:500,
     y1:0,
     x2:500,
-    y2:200,
+    y2:50,
     fill:0,
     speed:5,
 }
 let hookImg;
+let hookImgSize = 50;
 
 /**
  * Load files
@@ -49,16 +51,45 @@ function preload() {
 function setup() {
     createCanvas(windowWidth,windowHeight);
     noStroke();
+    // Randomize fish
+    resetFish();
 }
-
 
 /**
  * Draw objects on the canvas
 */
 function draw() {
     background(59, 126, 235);
+    simulation();   
+}
+
+function simulation(){
+    isFishCaught();
     move();
     display();
+}
+
+function isFishCaught(){
+    // Get the x value of the fish's mouth
+    let fishMouth = fish.x + fish.w/2;
+    if(!fish.isFacingRight){
+        fishMouth = fish.x - fish.w/2;
+    }
+    // If the fishing hook is close enough to the hook, the fish is caught
+    if(dist(fishingLine.x2, fishingLine.y2 + hookImgSize, fishMouth, fish.y) < 15){
+        fish.isCaught = true;
+    }
+}
+
+function resetFish(){
+    fish.x = 0 - fish.w;
+    fish.y = random(windowHeight/4, windowHeight);
+    fish.w = random(200, 500);
+    fish.fill.r = random(0,255);
+    fish.fill.g = random(0,255);
+    fish.fill.b = random(0,255);
+    fish.h = fish.w/2;
+    fish.isCaught = false;
 }
 
 function move(){
@@ -77,6 +108,21 @@ function move(){
     // Let the hook sink
     fishingLine.y2 ++;
 
+    // If the fish is caught, follow the hook
+    if(fish.isCaught){
+        if(fish.isFacingRight){
+            fish.x = fishingLine.x2 - fish.w/2;
+        }
+        else{
+            fish.x = fishingLine.x2 + fish.w/2;
+        }
+        fish.y = fishingLine.y2 + hookImgSize;
+        // Check if the fish is off screen
+        if(fish.y - fish.h/2 < 0){
+            resetFish();
+        }
+        return;
+    }
     // Move the fish
     if(fish.isFacingRight){
         // Move to the right
@@ -97,7 +143,10 @@ function move(){
 }
 
 function mouseMoved(){
-    fishingLine.y2 -= fishingLine.speed;
+    // Raise the fishing line when the mouse is moved
+    if(fishingLine.y2 > -100){
+        fishingLine.y2 -= fishingLine.speed;
+    } 
 }
 
 function display(){
@@ -125,5 +174,5 @@ function display(){
     stroke(0);
     line(fishingLine.x1, fishingLine.y1, fishingLine.x2, fishingLine.y2);
     noStroke();
-    image(hookImg, fishingLine.x2 - 25, fishingLine.y2, 50, 50);
+    image(hookImg, fishingLine.x2 - hookImgSize/2, fishingLine.y2, hookImgSize, hookImgSize);
 }
