76404b644cd1611318f365e437a34296c6613135
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-10-24 13:17:32 -0400

    starting juggling garden activity

---
+++ activities/07-juggle-garden/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * Juggle Garden
+ * Ryan Bujold
+ * 
+ * Making a juggling simulation
+ */
+
+"use strict";
+
+/**
+ * Load neccessary files
+*/
+function preload() {
+
+}
+
+/**
+ * Setup objects and the canvas
+*/
+function setup() {
+
+}
+
+/**
+ * Draw on the canvas
+*/
+function draw() {
+
+}
