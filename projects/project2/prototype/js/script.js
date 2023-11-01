3da5146cc4b4fe66c952d66ac55784fa16698e21
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-11-01 16:51:11 -0400

    began drawing objects
    drawing objects to the canvas before manipulating them.

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -7,14 +7,43 @@
 
 "use strict";
 
+let canvasWidth = 1600
+let canvasHeight = 800;
+
+let user = {
+    x: canvasWidth / 2,
+    y: canvasHeight / 2,
+    rotation: 0,
+    size: 50,
+}
+
+let box = {
+    x: 300,
+    y: 300,
+    w: 200,
+    h: 200,
+}
+
 function preload() {
 
 }
 
 function setup() {
-
+    createCanvas(canvasWidth, canvasHeight);
 }
 
 function draw() {
+    background(0);
 
+    // Draw the user
+    fill(200, 0, 0);
+    ellipse(user.x, user.y, user.size);
+    // Draw a pointer for the direction the user is facing
+    stroke(0, 200, 0)
+    line(user.x, user.y, user.x, user.y - 25);
+    noStroke();
+
+    // Draw a box
+    fill(200);
+    rect(box.x, box.y, box.w, box.y);
 }
