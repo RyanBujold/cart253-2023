e0639d5de64402eef9cb91ccd021762739997c87
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-11-01 18:07:39 -0400

    added user sight area and rotation
    added a triangle that represents the user's visibility. The user can now also rotate their view using the arrow keys.

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -15,6 +15,10 @@ let user = {
     y: canvasHeight / 2,
     rotation: 0,
     size: 50,
+    sight: {
+        range: 45,
+        distance: 500,
+    }
 }
 
 let box = {
@@ -30,20 +34,38 @@ function preload() {
 
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
+    angleMode(DEGREES);
 }
 
 function draw() {
     background(0);
 
+    // Move the user
+    moveUser();
+    // Draw the user's sight
+    fill(0, 0, 200);
+    triangle(user.x, user.y, 
+        user.x - sin(user.rotation + user.sight.range) * user.sight.distance, user.y - cos(user.rotation + user.sight.range) * user.sight.distance, 
+        user.x - sin(user.rotation - user.sight.range) * user.sight.distance, user.y - cos(user.rotation - user.sight.range) * user.sight.distance);
     // Draw the user
     fill(200, 0, 0);
     ellipse(user.x, user.y, user.size);
     // Draw a pointer for the direction the user is facing
-    stroke(0, 200, 0)
-    line(user.x, user.y, user.x, user.y - 25);
+    stroke(0, 200, 0);
+    line(user.x, user.y, user.x - sin(user.rotation) * 50, user.y - cos(user.rotation) * 50);
     noStroke();
 
     // Draw a box
     fill(200);
     rect(box.x, box.y, box.w, box.y);
+}
+
+function moveUser() {
+    // Rotate the user right or left
+    if (keyIsDown(LEFT_ARROW)) {
+        user.rotation += 1;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        user.rotation -= 1;
+    }
 }
