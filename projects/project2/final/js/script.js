8906fe8eabb7a81d4bcca1a571230355f5d0d65d
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-11-14 13:28:23 -0500

    refactored code into a user class

--- projects/project2/final/js/script.js
+++ projects/project2/final/js/script.js
@@ -10,18 +10,7 @@
 let canvasWidth = 1600
 let canvasHeight = 800;
 
-let user = {
-    x: canvasWidth / 2,
-    y: canvasHeight / 2,
-    rotation: 0,
-    size: 50,
-    flashlight: {
-        range: 30,
-        distance: 1000,
-    },
-    moveSpeed: 3,
-    turnSpeed: 1.5,
-}
+let user;
 
 function preload() {
 
@@ -30,36 +19,27 @@ function preload() {
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
     angleMode(DEGREES);
+    noStroke();
+
+    // Initialize the user
+    user = new User(canvasWidth / 2, canvasHeight / 2);
 }
 
 function draw() {
     background(0);
 
     // Move the user
-    moveUser();
+    user.move();
+
     // Draw the user
-    fill(200, 0, 0);
-    ellipse(user.x, user.y, user.size);
-    // Draw a pointer for the direction the user is facing
-    stroke(0, 200, 0);
-    line(user.x, user.y, user.x - sin(user.rotation) * 50, user.y - cos(user.rotation) * 50);
-    noStroke();
+    user.display();
 
     // Draw a gray box 
     fill(50);
     rect(600, 100, 100, 100);
 
-    // Draw the user's sight
-    fill(255, 255, 255, 50);
-    let leftSightPoint = {
-        x: user.x - sin(user.rotation + user.flashlight.range) * user.flashlight.distance,
-        y: user.y - cos(user.rotation + user.flashlight.range) * user.flashlight.distance,
-    }
-    let rightSightPoint = {
-        x: user.x - sin(user.rotation - user.flashlight.range) * user.flashlight.distance,
-        y: user.y - cos(user.rotation - user.flashlight.range) * user.flashlight.distance,
-    }
-    triangle(user.x, user.y, leftSightPoint.x, leftSightPoint.y, rightSightPoint.x, rightSightPoint.y);
+    // Draw the user's flashlight
+    user.displayFlashlight();
 
     // Draw a black box
     fill(0);
@@ -70,23 +50,4 @@ function draw() {
     fill(shade);
     ellipse(1000, 500, 50);
 
-}
-
-function moveUser() {
-    // Rotate the user right or left
-    if (keyIsDown(LEFT_ARROW)) {
-        user.rotation += user.turnSpeed;
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-        user.rotation -= user.turnSpeed;
-    }
-    // Move forward and backwards
-    if (keyIsDown(UP_ARROW)) {
-        user.x -= sin(user.rotation) * user.moveSpeed;
-        user.y -= cos(user.rotation) * user.moveSpeed;
-    }
-    if (keyIsDown(DOWN_ARROW)) {
-        user.x += sin(user.rotation) * user.moveSpeed;
-        user.y += cos(user.rotation) * user.moveSpeed;
-    }
 }
---
+++ projects/project2/final/js/user.js
@@ -0,0 +1,61 @@
+class User {
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.rotation = 0;
+        this.size = 50;
+        this.flashlight = {
+            range: 30,
+            distance: 500,
+        };
+        this.moveSpeed = 3;
+        this.turnSpeed = 1.5;
+    }
+
+    move() {
+        // Rotate the user right or left
+        if (keyIsDown(LEFT_ARROW)) {
+            this.rotation += this.turnSpeed;
+        }
+        if (keyIsDown(RIGHT_ARROW)) {
+            this.rotation -= this.turnSpeed;
+        }
+        // Move forward and backwards
+        if (keyIsDown(UP_ARROW)) {
+            this.x -= sin(this.rotation) * this.moveSpeed;
+            this.y -= cos(this.rotation) * this.moveSpeed;
+        }
+        if (keyIsDown(DOWN_ARROW)) {
+            this.x += sin(this.rotation) * this.moveSpeed;
+            this.y += cos(this.rotation) * this.moveSpeed;
+        }
+    }
+
+    display() {
+        push();
+        // Draw the user
+        fill(200, 0, 0);
+        ellipse(this.x, this.y, this.size);
+
+        // Draw a pointer for the direction the user is facing
+        stroke(0, 200, 0);
+        line(this.x, this.y, this.x - sin(this.rotation) * 50, this.y - cos(this.rotation) * 50);
+        noStroke();
+        pop();
+    }
+
+    displayFlashlight() {
+        // Draw the user's sight
+        fill(255, 255, 255, 50);
+        let leftSightPoint = {
+            x: this.x - sin(this.rotation + this.flashlight.range) * this.flashlight.distance,
+            y: this.y - cos(this.rotation + this.flashlight.range) * this.flashlight.distance,
+        }
+        let rightSightPoint = {
+            x: this.x - sin(this.rotation - this.flashlight.range) * this.flashlight.distance,
+            y: this.y - cos(this.rotation - this.flashlight.range) * this.flashlight.distance,
+        }
+        triangle(this.x, this.y, leftSightPoint.x, leftSightPoint.y, rightSightPoint.x, rightSightPoint.y);
+    }
+}
