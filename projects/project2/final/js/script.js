d002e1a3b7e2751c834b17dd3627c650b7f29b0d
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-11-14 15:28:01 -0500

    figured out how to make shadows

--- projects/project2/final/js/script.js
+++ projects/project2/final/js/script.js
@@ -54,15 +54,58 @@ function draw() {
     ellipse(1000, 500, 50);
 
     // Draw the box's shadow
-    let cx = 500;
-    let cy = 300;
+    let s1 = drawShadow(300, 300);
+    let s2 = drawShadow(500, 300);
+    let s3 = drawShadow(300, 500);
+    let s4 = drawShadow(500, 500);
+
+    // Shadow top
+    fill(0,200,0);
+    beginShape();
+    vertex(300, 300);
+    vertex(s1.x, s1.y);
+    vertex(s2.x, s2.y);
+    vertex(500, 300);
+    endShape(CLOSE);
+    // Shadow left
+    fill(200,0,0);
+    beginShape();
+    vertex(300, 300);
+    vertex(s1.x, s1.y);
+    vertex(s3.x, s3.y);
+    vertex(300, 500);
+    endShape(CLOSE);
+    // Shadow right
+    fill(0,0,200);
+    beginShape();
+    vertex(300, 500);
+    vertex(s3.x, s3.y);
+    vertex(s4.x, s4.y);
+    vertex(500, 500);
+    endShape(CLOSE);
+    // Shadow bottom
+    fill(0,200,200);
+    beginShape();
+    vertex(500, 500);
+    vertex(s4.x, s4.y);
+    vertex(s2.x, s2.y);
+    vertex(500, 300);
+    endShape(CLOSE);
+
+}
+
+function drawShadow(cx, cy) {
     let hoff = 100;
-    let hx = map(user.x, cx-canvasWidth, cx+canvasWidth, -hoff, hoff);
-    let hy = map(user.y, cy-canvasHeight, cy+canvasHeight, -hoff, hoff);
+    let hx = map(user.x, cx - canvasWidth, cx + canvasWidth, -hoff, hoff);
+    let hy = map(user.y, cy - canvasHeight, cy + canvasHeight, -hoff, hoff);
     let shadow = 100;
-    let sx = hx*-shadow;
-    let sy = hy*-shadow;
+    let sx = hx * -shadow;
+    let sy = hy * -shadow;
     stroke(0, 0, 255);
-    line(500, 300, cx+sx, cy+sy);
+    line(cx, cy, cx + sx, cy + sy);
     noStroke();
+    return {
+        x: cx + sx,
+        y: cy + sy,
+    }
 }
